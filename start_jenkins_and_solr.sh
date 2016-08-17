803dffdff5b8d29bcbd66a6cb0be5a39df21fc43
James Strassburg <jstrassburg@gmail.com>
2016-08-17 08:38:12 -0500

    Copying ssh keys for the vagrant boxes to the jenkins_home directory so they're accessible by Jenkins to ssh to the hosts. Adding a test build to ssh ifconfig to each solr box.

--- start_jenkins_and_solr.sh
+++ start_jenkins_and_solr.sh
@@ -2,6 +2,9 @@
 
 vagrant up
 
+cp .vagrant/machines/prod/virtualbox/private_key jenkins_home/prod_private_key
+cp .vagrant/machines/stage/virtualbox/private_key jenkins_home/stage_private_key
+
 jenkins_home=`pwd`/jenkins_home
 docker run -p 8080:8080 -p 50000:50000 -v $jenkins_home:/var/jenkins_home jenkins
 
